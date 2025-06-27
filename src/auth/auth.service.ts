d88062779208ecd208c093b5af2d5d311811c9da
Belat <enochbelawu8@gmail.com>
2025-06-27 12:37:01 +0100

    feat: add authentication DTO and update auth service methods; include validation pipe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signin() {
+    signin(dto: AuthDto) {
         return { msg: "I am signed in" };
     }
-    signup() {
+    signup(dto: AuthDto) {
         return { msg: "I am signed up" };
     }
 }
